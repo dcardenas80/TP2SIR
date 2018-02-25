48581b99581406f52977c6ed469355adfb703aa3
Diego <dym2312@hotmail.com>
2018-02-25 20:40:30 +0100

    ToString method for the class Person, method to show the Persons on DB

--- tpSIR2/src/main/java/fr/istic/sir/tp2/datamodel/Person.java
+++ tpSIR2/src/main/java/fr/istic/sir/tp2/datamodel/Person.java
@@ -1,6 +1,5 @@
 package fr.istic.sir.tp2.datamodel;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,54 +19,67 @@ public class Person {
 	private String lastName;
 	private List<Home> homes = new ArrayList<Home>();
 	private List<Person> friends = new ArrayList<Person>();
+
 	@Id
 	@GeneratedValue
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	
 	@ManyToMany(targetEntity = Person.class)
 	public List<Person> getFriends() {
 		return friends;
 	}
+
 	public void setFriends(List<Person> friends) {
 		this.friends = friends;
 	}
-	
+
 	@OneToMany(cascade = CascadeType.PERSIST)
 	@JoinColumn(name = "person_id")
 	public List<Home> getHomes() {
 		return homes;
 	}
+
 	public void addHomes(Home homeId) {
 		homes.add(homeId);
 	}
+
 	public void setHomes(List<Home> homes) {
 		this.homes = homes;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
-	
-	
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", last name:" + lastName + ", email: " + email + "]";
+	}
+
 }
--- tpSIR2/src/main/java/jpa/JpaTest.java
+++ tpSIR2/src/main/java/jpa/JpaTest.java
@@ -16,15 +16,16 @@ import fr.istic.sir.tp2.datamodel.Person;
 
 public class JpaTest {
 	private EntityManager manager;
+
 	public JpaTest(EntityManager manager) {
 		this.manager = manager;
 	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		EntityManagerFactory factory =   
- 			 Persistence.createEntityManagerFactory("mysql");
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("mysql");
 		EntityManager manager = factory.createEntityManager();
 		JpaTest test = new JpaTest(manager);
 		EntityTransaction tx = manager.getTransaction();
@@ -37,52 +38,63 @@ public class JpaTest {
 		}
 		tx.commit();
 		test.listEmployees();
-			
-   	 manager.close();
+		test.listPerson();
+
+		manager.close();
 		System.out.println(".. done");
 	}
+
 	private void createEmployees() {
 		int numOfEmployees = manager.createQuery("Select a From Employee a", Employee.class).getResultList().size();
 		if (numOfEmployees == 0) {
 			Department department = new Department("java");
 			manager.persist(department);
-			manager.persist(new Employee("Jakab Gipsz",department));
-			manager.persist(new Employee("Captain Nemo",department));
+			manager.persist(new Employee("Jakab Gipsz", department));
+			manager.persist(new Employee("Captain Nemo", department));
 		}
 	}
-	
+
 	private void createPersons() {
-		 Heater heaterOne = new Heater();
-		 heaterOne.setHeaterDescription("petit");
-	     Person diego = new Person();
-		 Heater heaterTwo = new Heater();
-		 heaterTwo.setHeaterDescription("grand");
-		 ElectronicDevice electronicDevice = new ElectronicDevice();
-		 electronicDevice.setName("Oven");
-		 ElectronicDevice electronicDeviceTwo = new ElectronicDevice();
-		 electronicDeviceTwo.setName("Refrigerator");
-		 Home home  = new Home();
-		 home.setName("Casa 1");
-		 home.addHeater(heaterOne);
-		 home.addHeater(heaterTwo);
-		  diego.setName("Diego");
-		  diego.setLastName("Cardenas");
-		  diego.setEmail("ejemplo@test.com");
-		  diego.addHomes(home);
-		  home.addElectronicDevice(electronicDevice);
-		  home.addElectronicDevice(electronicDeviceTwo);
-		  manager.persist(heaterOne);
-		  manager.persist(heaterTwo);
-		  manager.persist(electronicDevice);
-		  manager.persist(electronicDeviceTwo);
-		  manager.persist(home);
-		  manager.persist(diego);
+		Heater heaterOne = new Heater();
+		heaterOne.setHeaterDescription("petit");
+		Person diego = new Person();
+		Heater heaterTwo = new Heater();
+		heaterTwo.setHeaterDescription("grand");
+		ElectronicDevice electronicDevice = new ElectronicDevice();
+		electronicDevice.setName("Oven");
+		ElectronicDevice electronicDeviceTwo = new ElectronicDevice();
+		electronicDeviceTwo.setName("Refrigerator");
+		Home home = new Home();
+		home.setName("Casa 1");
+		home.addHeater(heaterOne);
+		home.addHeater(heaterTwo);
+		diego.setName("Diego");
+		diego.setLastName("Cardenas");
+		diego.setEmail("ejemplo@test.com");
+		diego.addHomes(home);
+		home.addElectronicDevice(electronicDevice);
+		home.addElectronicDevice(electronicDeviceTwo);
+		manager.persist(heaterOne);
+		manager.persist(heaterTwo);
+		manager.persist(electronicDevice);
+		manager.persist(electronicDeviceTwo);
+		manager.persist(home);
+		manager.persist(diego);
 	}
+
 	private void listEmployees() {
 		List<Employee> resultList = manager.createQuery("Select a From Employee a", Employee.class).getResultList();
 		System.out.println("num of employess:" + resultList.size());
 		for (Employee next : resultList) {
 			System.out.println("next employee: " + next);
+		}
+	}
+
+	private void listPerson() {
+		List<Person> resultList = manager.createQuery("Select a from Person a", Person.class).getResultList();
+		System.out.println("num of persons" + resultList.size());
+		for (Person person : resultList) {
+			System.out.println("next Person: " + person);
 		}
 	}
 }
