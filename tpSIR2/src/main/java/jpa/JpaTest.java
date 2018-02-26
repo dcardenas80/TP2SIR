e699d05e51b1cd2ebcaae48844674aff532efa57
Diego <dym2312@hotmail.com>
2018-02-26 18:08:51 +0100

    Deletion of classes Department and Employee

--- tpSIR2/src/main/java/jpa/JpaTest.java
+++ tpSIR2/src/main/java/jpa/JpaTest.java
@@ -7,9 +7,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import fr.istic.sir.tp2.datamodel.Department;
 import fr.istic.sir.tp2.datamodel.ElectronicDevice;
-import fr.istic.sir.tp2.datamodel.Employee;
 import fr.istic.sir.tp2.datamodel.Heater;
 import fr.istic.sir.tp2.datamodel.Home;
 import fr.istic.sir.tp2.datamodel.Person;
@@ -31,28 +29,17 @@ public class JpaTest {
 		EntityTransaction tx = manager.getTransaction();
 		tx.begin();
 		try {
-			test.createEmployees();
 			test.createPersons();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		tx.commit();
-		test.listEmployees();
 		test.listPerson();
 
 		manager.close();
 		System.out.println(".. done");
 	}
 
-	private void createEmployees() {
-		int numOfEmployees = manager.createQuery("Select a From Employee a", Employee.class).getResultList().size();
-		if (numOfEmployees == 0) {
-			Department department = new Department("java");
-			manager.persist(department);
-			manager.persist(new Employee("Jakab Gipsz", department));
-			manager.persist(new Employee("Captain Nemo", department));
-		}
-	}
 
 	private void createPersons() {
 		Heater heaterOne = new Heater();
@@ -82,13 +69,6 @@ public class JpaTest {
 		manager.persist(diego);
 	}
 
-	private void listEmployees() {
-		List<Employee> resultList = manager.createQuery("Select a From Employee a", Employee.class).getResultList();
-		System.out.println("num of employess:" + resultList.size());
-		for (Employee next : resultList) {
-			System.out.println("next employee: " + next);
-		}
-	}
 
 	private void listPerson() {
 		List<Person> resultList = manager.createQuery("Select a from Person a", Person.class).getResultList();
